70d672767fc310ff36566acfcf03f47cb875bfec
yshany <15618711275@163.com>
2017-03-19 17:16:37 -0400

    Add files via upload

---
+++ base_line.R
@@ -0,0 +1,17 @@
+library(readr)
+setwd('C:/Users/YSHAN/Documents/ads/pj3')
+data=read_csv('sift_features.csv')
+library(xgboost)
+label=read.csv('labels.csv')
+param=list(
+  colsample_bytree=1/2000,
+  objective='binary:logistic',
+  max.depth=1,
+  eval_metric='error'
+)
+dtrain=xgb.DMatrix(data=data.matrix(t(data)),label=label[,1])
+a=xgb.cv(data=dtrain,params = param,nfold=5,nrounds=3165)
+#D:\Program Files\Java\jdk1.8.0_121\
+model=xgb.train(data=dtrain,params = param,nfold=5,nrounds=3165)
+
+

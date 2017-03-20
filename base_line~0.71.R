62915d3dbfdbc12f6014d02516360b2beefa8d78
yshany <15618711275@163.com>
2017-03-20 14:26:32 -0400

    Update and rename base_line.R to base_line~0.71.R

--- base_line~0.71.R
+++ base_line~0.71.R
@@ -12,6 +12,6 @@ param=list(
 dtrain=xgb.DMatrix(data=data.matrix(t(data)),label=label[,1])
 a=xgb.cv(data=dtrain,params = param,nfold=5,nrounds=round(5000/0.632))
 #D:\Program Files\Java\jdk1.8.0_121\
-model=xgb.train(data=dtrain,params = param,nfold=5,nrounds=round(5000/0.632))
+model=xgb.train(data=dtrain,params = param,nrounds=round(5000/0.632))
 
 

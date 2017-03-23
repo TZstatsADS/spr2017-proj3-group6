3c358470d07a37625057b20f5922be72f0765114
tangshuyi <st3037@columbia.edu>
2017-03-23 13:28:22 -0400

    Add files via upload

--- lbpfeature.m
+++ lbpfeature.m
@@ -1,41 +1,6 @@
-% Extract SIFT features for poddle vs. fried chicken image set
-% Require vlfeat-0.9.20
-% Adapted codes from http://www.vlfeat.org/applications/caltech-101-code.html
-% run('~/Documents/MATLAB/vlfeat-0.9.20/toolbox/vl_setup'
-
 conf.calDir = './' ; % calculating directory
 conf.dataDir = './images/' ; % data (image) directory 
-conf.outDir = './output2/'; % output directory
-conf.numWords = 5000 ; % vocabulary size
-conf.numSpatialX = 1 ; % spatial histogram configuration
-conf.numSpatialY = 1 ;
-conf.quantizer = 'kdtree' ; % search structure
 
-conf.prefix = 'sift_pf' ;
-conf.randSeed = 1 ;
-
-conf.descrPath = fullfile(conf.outDir,[conf.prefix '-descr.mat']);
-conf.vocabPath = fullfile(conf.outDir, [conf.prefix '-vocab.mat']);
-conf.histPath = fullfile(conf.outDir, [conf.prefix '-hists.mat']) ;
-conf.modelPath = fullfile(conf.outDir, [conf.prefix '-model.mat']) ;
-conf.resultPath = fullfile(conf.outDir, [conf.prefix '-result']) ;
-
-
-conf.descrPath = fullfile(conf.outDir, [conf.prefix '-selectedDescr.mat']);
-conf.framePath = fullfile(conf.outDir, [conf.prefix '-selectedFrame.mat']);
-
-randn('state',conf.randSeed) ;
-rand('state',conf.randSeed) ;
-
-
-model.numSpatialX = conf.numSpatialX ;
-model.numSpatialY = conf.numSpatialY ;
-model.quantizer = conf.quantizer ;
-model.vocab = [] ;
-
-% --------------------------------------------------------------------
-%                                                     Train vocabulary
-% --------------------------------------------------------------------
 
 file = fopen('train_img_names.csv');
 imagenames = textscan(file, '%s %*[^\n]');
@@ -44,14 +9,6 @@ imagenames = imagenames{:};
 imagenames = imagenames(2:end);
 [nfiles, s] = size(imagenames);
 
-
-
-  % Get some SIFT descriptors to train the dictionary
-  descrs = {} ;
-  frame = {};
-  img_names = {};
-  %for ii = 1:length(selTrainFeats)
-  %for ii = 1:nfiles
   for ii = 1:nfiles
     fprintf('Processing dictionary %s (%.2f %%)\n', imagenames{ii}, 100 * ii /nfiles) ;
     currentFileName = imagenames{ii};
@@ -72,5 +29,5 @@ lbpFeatures = reshape(lbpCellHists,1,[]);
 W(ii,:)=lbpFeatures;
   end
  
- csvwrite("fea.csv",W) 
+ csvwrite('feature_lbp.csv',W) 
   

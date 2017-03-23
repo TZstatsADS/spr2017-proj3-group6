dc9ff83b46ddee20752f9ed9c8c0d1baeaef7158
tangshuyi <st3037@columbia.edu>
2017-03-23 18:47:15 -0400

    Delete group3_test.R

--- lib/group3_test.R
+++
@@ -1,8 +0,0 @@
-
-test <- function(model,feature){
-        base.model <- models$baseline 
-        adv.model <- models$adv
-        base.prediction <- predict(base.model,feature)
-        adv.prediction <- predict(adv.model,feature)
-        return(list(baseline=base.prediction,adv=adv.prediction))
-}
